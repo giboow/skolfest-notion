08bc7b8f8d0c78fa9d7dfad71d6fd8b41a33fd8a
Philippe Gibert <philippe.gibert@gmail.com>
2023-02-04 11:57:04 +0100

    Homepage display post in cards

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
-        <Main />
-        <NextScript />
+      <body className="body">
+        <header className="header">
+          Header
+        </header>
+        <div className="content">
+          <Main />
+          <NextScript />
+        </div>
       </body>
     </Html>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { BlogPost } from "@/@types/schema";
+import CardComponent from "@/components/common/card.component";
 import NotionService from "@/services/notion.service";
 import { InferGetStaticPropsType } from "next";
 import Head from "next/head";
@@ -19,7 +20,8 @@ export const getStaticProps = async () => {
 const Home = ({posts}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
-      {posts.map((post: BlogPost) => (post.title))}
+      
+      {posts.map((post: BlogPost) => (<CardComponent className="m-1" title={post.title} content={post.description} link="coucou" />))}
     </>
   )
 }
